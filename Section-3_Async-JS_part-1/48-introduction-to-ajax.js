295e04b9f9eb2ff539550c5e05552e282ca28f90
Ann-Fox <fx8565@mail.ru>
2026-03-12 14:46:09 +0500

    section-3: AJAX - GET, POST

--- Section-3_Async-JS_part-1/48-introduction-to-ajax.js
+++ Section-3_Async-JS_part-1/48-introduction-to-ajax.js
@@ -1,4 +1,5 @@
-const btn = document.querySelector('button');
+const btn = document.querySelector('.btn-get-posts');
+const btnAddNewPost = document.querySelector('.btn-add-post');
 const container = document.querySelector('.container');
 
 function getPosts(callback) {
@@ -19,24 +20,49 @@ function getPosts(callback) {
     xhr.send();
 };
 
+function createPost(body, callback) {
+    const xhr = new XMLHttpRequest();
+    // console.log(xhr);
+    xhr.open('POST', 'https://jsonplaceholder.typicode.com/posts');
+    xhr.addEventListener('load', () => {
+        // console.log(xhr.responseText);
+        const response = JSON.parse(xhr.responseText);
+        // console.log(response);
+        callback(response)
+    });
+
+    xhr.setRequestHeader('Content-type', 'application/json; charset=UTF-8')
+
+    xhr.addEventListener('error', () => {
+        console.log('error')
+    });
+
+    xhr.send(JSON.stringify(body));
+};
+
+function cardTemplate(post) {
+    const card = document.createElement('div');
+    card.classList.add('card');
+    const cardBody = document.createElement('div');
+    cardBody.classList.add('card-body');
+    const title = document.createElement('h5');
+    title.classList.add('card-title');
+    title.textContent = post.title;
+    const body = document.createElement('p');
+    body.classList.add('class-text');
+    body.textContent = post.body;
+    cardBody.appendChild(title);
+    cardBody.appendChild(body);
+    card.appendChild(cardBody)
+    return card;
+}
+
 function renderPost(response) {
     const fragment = document.createDocumentFragment();
 
     response.forEach(post => {
         // console.log(post);
-        const card = document.createElement('div');
-        card.classList.add('card');
-        const cardBody = document.createElement('div');
-        cardBody.classList.add('card-body');
-        const title = document.createElement('h5');
-        title.classList.add('card-title');
-        title.textContent = post.title;
-        const body = document.createElement('p');
-        body.classList.add('class-text');
-        body.textContent = post.body;
-        cardBody.appendChild(title);
-        cardBody.appendChild(body);
-        card.appendChild(cardBody)
+        const card = cardTemplate(post)
         fragment.appendChild(card)
         // console.log(fragment)
     });
@@ -46,3 +72,36 @@ function renderPost(response) {
 btn.addEventListener('click', () => {
     getPosts(renderPost)
 })
+
+btnAddNewPost.addEventListener('click', (e) => {
+    console.log(e.target);
+    const newPost = {
+        title: 'foo',
+        body: 'bar',
+        userId: 1,
+    }
+
+    createPost(newPost, response => {
+        const post = cardTemplate(response)
+        container.insertAdjacentElement('afterbegin', post)
+    })
+
+})
+
+// CORS
+function getGmail(callback) {
+    const xhr = new XMLHttpRequest();
+    // console.log(xhr);
+    xhr.open('GET', 'http://gmail.com');
+    xhr.addEventListener('load', () => {
+        console.log(xhr.responseText);
+    
+    });
+
+    xhr.addEventListener('error', () => {
+        console.log('error')
+    });
+
+    xhr.send();
+};
+
