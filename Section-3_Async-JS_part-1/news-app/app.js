31c1efaa96c80f838e3eeca90911e0997166cd13
Ann-Fox <fx8565@mail.ru>
2026-03-16 16:05:23 +0500

    section-3: project News App (part 2)

--- Section-3_Async-JS_part-1/news-app/app.js
+++ Section-3_Async-JS_part-1/news-app/app.js
@@ -66,12 +66,22 @@ const newServece = (function () {
         topHeadlines(country = 'ua', cb) {
             http.get(`${apiUrl}/top-headlines?country=${country}&category=technology&apiKey=${apiKey}`, cb)
         },
-        everything(query, cd) {
-            http.get(`${apiUrl}/v2/everything?q=${query}&apiKey=${apiKey}
+        everything(query, cb) {
+            http.get(`${apiUrl}/everything?q=${query}&apiKey=${apiKey}
 `, cb)
         },
     }
 })();
+
+// Element UI
+const form = document.forms['newsControls'];
+const countrySelect = form.elements['country'];
+const searchInput = form.elements['search']
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    loadNews();
+});
 
 // init selects
 document.addEventListener('DOMContentLoaded', function () {
@@ -81,26 +91,54 @@ document.addEventListener('DOMContentLoaded', function () {
 
 // Load news function (загрузка всех новостей)
 function loadNews() {
-    newServece.topHeadlines('us', onGetRasponse)
+    showLoader();
+
+    const country = countrySelect.value;
+    const searchText = searchInput.value;
+
+    if (!searchText) {
+        newServece.topHeadlines(country, onGetRasponse)
+    } else {
+        newServece.everything(searchText, onGetRasponse)
+        console.log(searchText);
+    }
+
 }
 
 // Function on get response from server (получить одну новость от сервера)
 function onGetRasponse(error, response) {
-    console.log(response.articles);
+    console.log(response);
+    // console.log(response.articles);
+    if (error) {
+        showAlert(error, 'red darken-3'); // css класс для оформления ошибки
+        return;
+    }
+    // если по запросу пользователя ничего не найдено, показать сообщение, что новостей нет по его запросу (по вашему запросу ничего не найдено)
+    if (!response.articles.length) {
+        // show empty msg
+        console.log(response.articles.length);
+        return;
+    }
+
     renderNews(response.articles)
 }
 
 // function render news
 function renderNews(news) {
+    removeLoader();
     const newsContainer = document.querySelector('.news-container .row');
-    console.log(newsContainer);
+
+    if (newsContainer.children.length) {
+        clearContainer(newsContainer)
+    }
+    // console.log(newsContainer);
     let fragment = '';
     news.forEach(newsItem => {
         // console.log(newsItem);
         const el = newTemplate(newsItem);
         fragment += el
     })
-    console.log(fragment);
+    // console.log(fragment);
     newsContainer.insertAdjacentHTML('afterbegin', fragment)
 }
 
@@ -122,4 +160,39 @@ function newTemplate({ urlToImage, title, url, description }) {
             </div>
         </div>
     `
+}
+
+function showAlert(msg, type = 'success') {
+    // console.log(msg, type);
+    M.toast({ html: msg, classes: type });
+}
+
+
+// function clear container (очистка контейнера при выборе новостей)
+function clearContainer(container) {
+    let child = container.lastElementChild;
+    while (child) {
+        container.removeChild(child)
+        child = container.lastElementChild
+    }
+}
+
+// show loader function
+function showLoader() {
+    document.body.insertAdjacentHTML('afterbegin',
+        `
+         <div class="progress m-0 fixed">
+            <div class="indeterminate"></div>
+        </div>
+  `
+    )
+}
+
+// remove loader function
+function removeLoader() {
+    const loader = document.querySelector('.progress');
+    if (loader) {
+        // console.log(loader);
+        loader.remove();
+    }
 }
